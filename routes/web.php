73460d79053dfb00afab368df5cd16fe263a763f
fikripcr <fikri@pcr.ac.id>
2025-10-24 07:24:13 +0700

    update data pelanggan

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\PelangganController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -30,9 +33,16 @@ Route::get('/about', function () {
     return view('halaman-about');
 });
 
-Route::get('/home', [HomeController::class,'index']);
+Route::get('/home', [HomeController::class,'index'])
+        ->name('home');
 
+Route::post('question/store', [QuestionController::class, 'store'])
+		->name('question.store');
 
+Route::get('dashboard', [DashboardController::class, 'index'])
+        ->name('dashboard');
+
+Route::resource('pelanggan', PelangganController::class);
 
 
 
